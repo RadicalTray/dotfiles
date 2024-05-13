2d78c9931b0d5024771cf9ea6760f62007b4a1e2
RadicalTray <radicaltray@gmail.com>
2024-05-13 19:57:47 +0700

    Change font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,7 +14,7 @@ config.front_end = 'WebGpu'
 -- config.font = wezterm.font("DejaVu Sans Mono")
 config.font = wezterm.font 'Hack'
 -- config.font = wezterm.font("Fira Code")
-config.font_size = 15
+config.font_size = 14
 
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
