8ee07a154f5802ecd6936adc77aa8331eed87ce8
RadicalTray <radicaltray@gmail.com>
2024-06-23 23:42:06 +0700

    update reminder.sh

--- .Scripts/reminder.sh
+++ .Scripts/reminder.sh
@@ -1,7 +1,7 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-echo ""
+echo
 echo -e " ${bold}MANUAL UPDATE:${normal} \u001b[32mneovim-nightly-bin\u001b[0m"
 echo    "         ${bold}CHECK:${normal} wezterm-git"
-echo ""
+echo
