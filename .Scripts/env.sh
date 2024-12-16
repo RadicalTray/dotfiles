8211503a88236b2a150e6609f67176739c80e60f
RadicalTray <radicaltray@gmail.com>
2024-12-16 20:08:10 +0700

    hi

--- .Scripts/env.sh
+++ .Scripts/env.sh
@@ -21,6 +21,10 @@ export EDITOR="/usr/bin/nvim"
 export SUDO_EDITOR="/usr/bin/nvim"
 export TERMINAL="/usr/bin/foot"
 if command -v luarocks 2>&1 >/dev/null; then
-    export LUAROCKS_CONFIG="$XDG_CONFIG_HOME/luarocks/config.lua"
+    if [[ -v $XDG_CONFIG_HOME ]]; then
+        export LUAROCKS_CONFIG="$XDG_CONFIG_HOME/luarocks/config.lua"
+    else
+        export LUAROCKS_CONFIG="$HOME/.config/luarocks/config.lua"
+    fi
     eval "$(luarocks path --no-bin)"
 fi
